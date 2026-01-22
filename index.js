99abf5138a8f7bd8ffb5a7677cc553f0f89898a5
benitomerda-cmd <benitomerda@icloud.com>
2026-01-22 08:57:17 +0300

    Create index.js
    Twitch-WizeBOT

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+const FACEIT_API_KEY = process.env.FACEIT_API_KEY;
+const FACEIT_NICK = "8benedictus8";
+
+app.get("/stats", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://open.faceit.com/data/v4/players?nickname=${FACEIT_NICK}`,
+      {
+        headers: {
+          Authorization: `Bearer ${FACEIT_API_KEY}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    const cs = data.games.cs2;
+    const elo = cs.faceit_elo;
+    const level = cs.skill_level;
+    const matches = cs.matches;
+    const wins = cs.wins;
+    const winrate = ((wins / matches) * 100).toFixed(1);
+
+    res.send(
+      `FACEIT: lvl ${level} | ELO ${elo} | WR ${winrate}%`
+    );
+  } catch (err) {
+    res.send("FACEIT временно недоступен");
+  }
+});
+
+app.listen(3000);
